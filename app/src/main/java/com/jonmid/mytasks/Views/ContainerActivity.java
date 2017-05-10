e9db780f1cf1a6a899ee73ff06bddd0dd47c25d1
AndresTeran <pipre94@gmail.com>
2017-05-10 13:27:58 -0500

    se inyecta el contaiernr del bottom bar

--- app/src/main/java/com/jonmid/mytasks/Views/ContainerActivity.java
+++ app/src/main/java/com/jonmid/mytasks/Views/ContainerActivity.java
@@ -1,9 +1,12 @@
 package com.jonmid.mytasks.Views;
 
+import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.jonmid.mytasks.R;
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.OnTabSelectListener;
 
 public class ContainerActivity extends AppCompatActivity {
 
@@ -11,5 +14,14 @@ public class ContainerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_container);
+
+        BottomBar bottomBar =(BottomBar) findViewById(R.id.id_bottombar);
+        bottomBar.setDefaultTab(R.id.tab_home);
+        bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
+            @Override
+            public void onTabSelected(@IdRes int tabId) {
+
+            }
+        });
     }
 }
